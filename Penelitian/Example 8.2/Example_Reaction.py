61d7f556912f231c5a5fb7f7c413b0d3aacca465
Mahardika-Pratama <mahardikajanuardi18@gmail.com>
2024-08-09 15:39:15 +0700

    consideration

--- Penelitian/Example 8.2/Example_Reaction.py
+++ Penelitian/Example 8.2/Example_Reaction.py
@@ -1,17 +1,6 @@
-#################################################################################
-# The Institute for the Design of Advanced Energy Systems Integrated Platform
-# Framework (IDAES IP) was produced under the DOE Institute for the
-# Design of Advanced Energy Systems (IDAES).
-#
-# Copyright (c) 2018-2023 by the software owners: The Regents of the
-# University of California, through Lawrence Berkeley National Laboratory,
-# National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
-# University, West Virginia University Research Corporation, et al.
-# All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
-# for full copyright and license information.
-#################################################################################
 """
-Example reaction property package for the hydrodealkylation of
+Example 8.2 in Seider's Product and Process Design Principles : Synthesis, Analysis, and Evaluation 4th Edition.
+reaction property package for the hydrodealkylation of
 toluene to form benzene, along with the side reaction to form diphenyl.
 
 This package assumes a single, ideal vapor phase with rate based conversion of
